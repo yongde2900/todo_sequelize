7cf3f85c6e3d07116076750be2e5f6dc2d2a9efc
yongde2900 <com414141@gmail.com>
2020-11-22 15:45:36 +0800

    modify: register feature

--- routes/models/users.js
+++ routes/models/users.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const db =require('../../models')
+const db = require('../../models')
 const User = db.User
 
 router.get('/login', (req, res) => {
@@ -18,8 +18,27 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
     const { name, email, password, confirmPassword } = req.body
-  User.create({ name, email, password })
-    .then(user => res.redirect('/'))
+    User.findOne({ where: { email } }).then(user => {
+        if (user) {
+            console.log('User already exists')
+            return res.render('register', {
+                name,
+                email,
+                password,
+                confirmPassword
+            })
+        }
+        return bcrypt
+            .genSalt(10)
+            .then(salt => bcrypt.hash(password, salt))
+            .then(hash => User.create({
+                name,
+                email,
+                password: hash
+            }))
+            .then(() => res.redirect('/'))
+            .catch(err => console.log(err))
+    })
 })
 
 router.get('/logout', (req, res) => {
