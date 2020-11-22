b37c99752cafe1fda1c602e5dde6d35dbdd252e8
yongde2900 <com414141@gmail.com>
2020-11-22 15:43:32 +0800

    feat: add detail page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express =require('express')
 const router = express.Router()
 const users = require('./models/users')
 const home = require('./models/home')
+const todos = require('./models/todos')
 
 router.use('/users', users)
+router.use('/todos', todos)
 router.use('/', home)
 
 
