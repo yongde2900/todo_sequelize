37de97f2922695863fc5fb52e253a579abc7807e
yongde2900 <com414141@gmail.com>
2020-11-22 17:26:33 +0800

    feat : add facebook login strategy

--- app.js
+++ app.js
@@ -8,15 +8,19 @@ const session = require('express-session')
 const flash = require('connect-flash')
 const usePassport = require('./config/passport')
 
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+  }
+
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(session({
-    secret: 'ThisIsMySecret',
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true
 }))
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router()
 const users = require('./models/users')
 const home = require('./models/home')
 const todos = require('./models/todos')
+const auth = require('./models/auth')
 const  { authenticator } = require('../middle-ware/auth')
 
 router.use('/users', users)
 router.use('/todos', authenticator, todos)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 
