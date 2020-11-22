f180461b815f6dc03fe9ec938f5860bf24982b8e
yongde2900 <com414141@gmail.com>
2020-11-22 16:42:12 +0800

    feat: add create page and feature

--- routes/models/todos.js
+++ routes/models/todos.js
@@ -3,6 +3,17 @@ const router = express.Router()
 const db =require('../../models')
 const Todo = db.Todo
 
+router.get('/new', (req, res) => {
+    res.render('new')
+})
+router.post('/', (req, res) => {
+    const UserId = req.user.id
+    const name = req.body.name
+    Todo.create({name, UserId})
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+})
+
 router.get('/:id', (req, res) => {
     const id = req.params.id
     return Todo.findByPk(id)
